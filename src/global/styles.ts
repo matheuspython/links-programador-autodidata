a057040804a47c6792fb68e17f78c564b0ddc3a4
matheuspython <ruivorobado@gmail.com>
2022-05-19 18:37:54 -0300

    estrutura inicial do projeto

---
+++ src/global/styles.ts
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const Globals = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html, body{
+    font-family: 'Roboto', sans-serif;
+  }
+`
