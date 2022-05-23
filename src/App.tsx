43a8247f3214a7206a0a69f09a96a2b0380339cf
matheuspython <ruivorobado@gmail.com>
2022-05-23 18:27:00 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Globals } from './global/styles'
-import { Header } from './components/Header';
-
-import { ThemeProvider } from 'styled-components';
-
-import dark from './global/themes/dark'
 import light from './global/themes/light'
 import { Main } from './components/Main';
 
 export const App:React.FC = () => {
-  const [theme, setTheme] = useState(dark)
-
-  const toggleTheme = () =>  setTheme(theme.title === 'light' ? dark : light)
-   
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Globals />
-      <Header toggleTheme={toggleTheme} />
       <Main/>
-    </ThemeProvider>
+    </>
   )
 }
 
